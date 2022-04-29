6a82e5dc5d62836b859bbe357395d3a60453e1b2
Victoria Okosyan <resemble@rambler.ru>
2022-04-29 23:30:01 +0300

    feat: test decision

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t = Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / k;
+  if (typeof sampleActivity === 'string' && sampleActivity !==null && sampleActivity !== undefined && t > 0 && t !== NaN && t !== Infinity) {
+    return Math.ceil(t);
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let newarray = [];
+  if (Array.isArray(members) && members.length > 0) {
+  for (let i = 0; i < members.length; i++) {
+   
+    
+  if (typeof members[i] === 'string') {
+    
+  newarray.push(members[i].trim()[0].toUpperCase());
+  }
+} if (newarray.length > 0) {
+  return newarray.sort().join('');
+} else {
+  return false;
+}
+} else {
+  return false;
+}
 }
 
 module.exports = {
