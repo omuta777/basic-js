3b34753483bb6f2cff4e790d5d6ef911668cc254
Victoria Okosyan <resemble@rambler.ru>
2022-05-01 00:37:30 +0300

    feat: test

--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  let season;
+
+  try {
+    date.getTime();
+ 
+   } catch(err) {
+    if (!arguments.length) {
+      return 'Unable to determine the time of year!';
+    } else {
+     
+       throw new Error("Invalid date!");
+     }
+  
+    }
+    season = date.getMonth();
+  
+
+  /*try {
+    date.getTime();
+ 
+   } catch(err) {
+     
+       throw new Error("Invalid date!");
+     }*/
+ 
+
+  if (season <=1 || season == 11) {
+    return 'winter';
+  } else if (season <= 4) {
+    return 'spring'
+  } else if (season <= 7) {
+    return 'summer'
+  } else if (season <= 10) {
+    return 'fall';
+  } else {
+    throw new Error("Invalid date!");
+  }
+
+return data;
+ 
 }
 
 module.exports = {
   getSeason
 };
+
+
+/*try {
+   if (date.getTime) {
+    season = date.getMonth();
+   }
+    
+  } catch(err) {
+    if (typeof data === 'undefined') {
+      return 'Unable to determine the time of year!';
+    } else  {
+      throw new Error("Invalid date!");
+    }
+
+  }*/
